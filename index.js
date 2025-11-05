fae7b7d990051667411b82bd71ad2f023a8e0c5c
sbisht <sbisht@opentext.com>
2025-11-06 00:06:56 +0530

    data added

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+const randomData={
+  location: 'New York',
+  temperature: '22°C',
+  condition: 'Sunny',
+  id: 12345,
+  users: [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Charlie' }
+  ],
+  products: [
+    { id: 1, name: 'Laptop', price: 999.99 },
+    { id: 2, name: 'Smartphone', price: 499.99 },
+    { id: 3, name: 'Tablet', price: 299.99 }
+  ]
+}
+
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.get('/status', (req, res) => {
-  res.json({ status: 'OK' });
+app.get('/data', (req, res) => {
+  res.json(randomData);
 })  
 
 app.get('/login', (req, res) => {
